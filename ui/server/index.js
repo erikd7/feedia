105b56f5c2bca06a195fbf4baa17df9b73931de8
erikd7 <eb.dietrich11@gmail.com>
2023-04-26 12:56:04 -0500

    Proxy frontend requests to backend

---
+++ ui/server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const setupProxy = require("./proxy.js");
+const path = require("path");
+
+const app = express();
+
+const staticPath = path.join(__dirname, "../dist");
+
+// Serve static files
+app.use(express.static(staticPath));
+
+setupProxy(app);
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
