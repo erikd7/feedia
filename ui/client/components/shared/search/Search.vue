187d5fa17dfe2c13613eddb9a0e1c4023f521af0
erikd7 <eb.dietrich11@gmail.com>
2023-09-25 12:41:09 -0400

    Refactor UI result options to use slots

--- ui/client/components/shared/search/Search.vue
+++ ui/client/components/shared/search/Search.vue
@@ -8,21 +8,31 @@
     ref="search-bar"
   >
     <template v-slot:option="slotProps">
-      <Option :option="slotProps.option.option" />
+      <MediaTypeSwitcher
+        :mediaType="slotProps.option.option.mediaType"
+        :componentsByMediaType="componentsByMediaType"
+        :passedProps="{
+          [slotProps.option.option.mediaType.toLowerCase()]:
+            slotProps.option.option,
+          showMediaType
+        }"
+      />
     </template>
   </SearchBar>
 </template>
 
 <script>
 import SearchBar from "./SearchBar";
-import Option from "./Option";
 import { mapActions, mapState, mapGetters } from "vuex";
 import { ROUTES } from "../../../util/constants/navigation";
-import { getSearchTerms } from "../../../util/constants/base";
+import { getSearchTerms, MEDIA_TYPES } from "../../../util/constants/base";
 import aggregateSearch from "../../../util/fetch/search";
+import BookOption from "../../book/BookOption.vue";
+import MovieOption from "../../movie/MovieOption.vue";
+import MediaTypeSwitcher from "../media-type/MediaTypeSwitcher.vue";
 
 export default {
-  components: { SearchBar, Option },
+  components: { SearchBar, MediaTypeSwitcher },
   props: {
     inputId: {
       type: String
@@ -38,6 +48,15 @@ export default {
     },
     searchPlaceholder() {
       return getSearchTerms(this.currentMediaTypes);
+    },
+    componentsByMediaType() {
+      return {
+        [MEDIA_TYPES.BOOK]: BookOption,
+        [MEDIA_TYPES.MOVIE]: MovieOption
+      };
+    },
+    showMediaType() {
+      return this.currentMediaTypes?.length > 1; //Show media type options if there are multiple media types
     }
   },
   methods: {
