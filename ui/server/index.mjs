56d97cda96753312767366114d1a0b21c88dd379
erikd7 <eb.dietrich11@gmail.com>
2023-10-04 20:52:44 -0400

    UI router improvements

--- ui/server/index.mjs
+++ ui/server/index.mjs
@@ -13,9 +13,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const staticPath = path.join(__dirname, "../dist");
 
-// Serve static files
+//Serve static files
 app.use(express.static(staticPath));
-
+//Handle all routes by sending the HTML file (makes Vue router work)
+app.get("*", (req, res) => {
+  res.sendFile(staticPath + "/index.html");
+});
 //Enable CORS for /api routes
 app.use(
   "/api",
@@ -31,6 +34,11 @@ setupTmdbProxy(app);
 
 //Proxy to internal APIs
 setupInternalProxy(app);
+
+//Redirect to root
+/*app.use((_req, res) => {
+  res.redirect("/");
+});*/
 
 // Start the server
 app.listen(4000, () => {
