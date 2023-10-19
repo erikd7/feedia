8fb2ac71e14de6b1630900e000470edbe9cc2c42
erikd7 <eb.dietrich11@gmail.com>
2023-10-18 20:31:23 -0400

    Create basic error boundary

---
+++ ui/client/components/shared/error/ErrorBoundary.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="error" class="middle error text-4xl">
+    <p>
+      <i class="text-red-600 fa fa-exclamation-triangle m-1" />
+      <a class="text-4xl">Shoot, something went wrong.</a>
+    </p>
+    <p class="text-lg">{{ error.message }}</p>
+  </div>
+  <slot v-else />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      error: null
+    };
+  },
+  errorCaptured(error, vm, info) {
+    this.error = error; //Set the error
+    console.error(error, vm, info); //Log the error to the console
+    return false; //Prevent error from propagating
+  }
+};
+</script>
