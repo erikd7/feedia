9acb0fead2aeb65946e0e0e03c201190874afa43
erikd7 <eb.dietrich11@gmail.com>
2023-11-19 20:08:46 -0500

    Generic Find landing page

--- ui/client/components/find/Find.vue
+++ ui/client/components/find/Find.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
     <Loading v-if="loading" message="Loading search results..." />
-    <SearchResults v-else-if="results" :results="results" />
+    <div v-else>
+      <SearchResults v-if="results" :results="results" />
+      <div
+        class="min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8"
+      >
+        <div class="text-center">
+          <h1
+            class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl"
+          >
+            Enter a search term
+          </h1>
+          <p class="mt-6 text-base leading-7 text-gray-600">
+            Books, movies, and more. All in one place.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,10 +28,6 @@ import { mapState } from "vuex";
 
 export default {
   components: { SearchResults, Loading },
-  props: {},
-  data() {
-    return {};
-  },
   computed: {
     ...mapState("search", ["results", "selection", "loading"])
   }
